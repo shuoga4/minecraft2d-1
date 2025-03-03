9613752738eb3a1675f0828c8fe419c7c78c342a
shuoga4 <shuoga0426@gmail.com>
2025-03-03 19:50:59 +0900

    init2

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,14 @@
+import jade.Window;
+
 public class HelloWorld {
     public static void main(String[] args) {
-
+        //まずはwindowクラスを作ってmainからwindow構造を切り離す。
+        //基本内部構造と外の出力をクラスで分けるのは、多分オブジェクト指向の仕様。
+        //jadeという名前は、エンジンの名前。
+        Window window = Window.get();
+        window.run();
+        //これから、ファクトリーメソッドでインスタンスの作成及び、runメソッドでwindowを作る。
+        //ここで、ファクトリーメソッドなのは、windowが複数作られるのを防ぐため。
+        //
     }
 }
--- src/jade/Window.java
+++ src/jade/Window.java
@@ -1,4 +1,66 @@
 package jade;
 
+import org.lwjgl.Version;
+import org.lwjgl.glfw.GLFWErrorCallback;
+
+import static org.lwjgl.glfw.GLFW.glfwDefaultWindowHints;
+import static org.lwjgl.glfw.GLFW.glfwInit;
+
 public class Window {
+    //さらに、シングルトン化を行う
+    //理由は、初期化時に読み込まれることを防ぎ、かつgetされたときのみ読み込まれるようにするためにここに内部静的クラスを作成し、
+    //さらに、パブリックのfinal staticで変数 Instance = new 外部クラス();と置くことで、このInstance変数が参照されたときのみ
+    //読み込まれる。
+
+    private int width,height;
+    private String title;
+
+    private Window(){
+        width = 1920;
+        height = 1080;
+        title = "Mario";
+    }
+
+    public static Window get(){
+        return WindowHolder.INSTANCE;
+    }
+
+    public void run(){
+        //とりあえず、これでversionの初期化。
+        //initとloopメソッドの作成。
+        System.out.println("This is LWJGL:" + Version.getVersion());
+
+        init();
+        loop();
+    }
+
+    public void init(){
+        //まず、エラーのコールバック先を作る。
+        //コールバックとは、lwjglでなにかエラーが起こったときにここにエラーが集まる。
+        GLFWErrorCallback.createPrint(System.err).set();
+        //これで、エラーが起こったときSystemのerrを通じてエラーを出すよという宣言。
+        if(!glfwInit()){
+            throw new IllegalStateException("init failed");
+        }
+
+        glfwDefaultWindowHints();
+
+    }
+
+    public void loop(){
+
+    }
+
+    public static class WindowHolder{
+        //そもそも、この内部クラスのスコープを理解していない。
+        //まあ少なくともパブリックかつプライベートコンストラクタだから、多分大丈夫。
+        //しかも、これ以外にもシングルトンの作り方があるみたい。
+        //内部クラスを作らない作り方だと
+        //private staticでWindow変数をつくってnullで初期化、get()で
+        //その変数がnullのときだけインスタンスを作って、あとは変数をリターンする。
+        //こっちのほうが理解はしやすい。
+        //こっちが複雑なのは、なれない内部クラスを使っているから。
+        //でもifとか使わなくてもいいからget()の可読性は上がってるかも。
+        private static final Window INSTANCE = new Window();
+    }
 }
